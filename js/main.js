3838002f0ba0872c7bad164dbe69c79217375f4e
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-04 20:00:10 +0200

    .

--- js/main.js
+++ js/main.js
@@ -16,12 +16,30 @@ function textById (id, my_content = "") {
   document.getElementById(id).textContent = my_content;
 }
 
-function isServerOnline (ip, port = 25565) {
-  var t = setInterval(runFunction,3000);
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
 }
 
-function runFunction () {
-  console.log("test");
+function isServerOnline (ip, port = 25565) {
+  console.log(httpGet("https://api.minetools.eu/ping/join.rpimc.xyz/25565"));
+  // var t = setInterval(runFunction,3000);
 }
 
 isServerOnline();
