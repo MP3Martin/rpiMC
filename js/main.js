6ac6dd7c55a2503d1ae8d93ffa6f456a15447f9a
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-04 19:48:55 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 /* -- Server status -- */
+
+function colorById (id, my_color = "black") {
+  if (my_color === "black") {
+    document.getElementById(id).style.color = "#000000";
+  } else if (my_color === "loading") {
+    document.getElementById(id).style.color = "#FFAE42";
+  } else if (my_color === "green") {
+    document.getElementById(id).style.color = "#00FF00";
+  } else if (my_color === "red") {
+    document.getElementById(id).style.color = "#FF0000";
+  }
+}
+
 function isServerOnline (ip, port = 25565) {
   var t = setInterval(runFunction,3000);
 }
@@ -9,4 +22,4 @@ function runFunction () {
 
 isServerOnline();
 
-document.getElementById("server-status-text").style.color = "#FFAE42";
+
