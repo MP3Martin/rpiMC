e0726ab1cb5606750d1859fc237c85150421508b
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-04 15:38:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* -- Server status -- */
 function isServerOnline (ip, port = 25565) {
-  var t = setInterval(runFunction,100);
+  var t = setInterval(runFunction,1);
 }
 
 function runFunction () {
