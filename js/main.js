8f33f35e6e18d9dc59ce07c0d363c5128e135fbe
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-05 19:25:06 +0200

    .

--- js/main.js
+++ js/main.js
@@ -83,20 +83,4 @@ $(document).ready(function(){
 });
 
 /* -- TOOLTIPS -- */
-Array.from(document.querySelectorAll('[tip]')).forEach(el => {
-  let tip = document.createElement('div');
-  tip.classList.add('tooltip');
-  tip.innerText = el.getAttribute('tip');
-  tip.style.transform =
-    'translate(' +
-      (el.hasAttribute('tip-left') ? 'calc(-100% - 5px)' : '15px') + ', ' +
-      (el.hasAttribute('tip-top') ? '-100%' : '0') +
-    ')';
-  el.appendChild(tip);
-  el.onmousemove = e => {
-    if (window.enable_red_tooltip === true) {
-      tip.style.left = e.clientX + 'px'
-      tip.style.top = e.clientY + 'px';
-    }
-  };
-});
+// NOTHING
